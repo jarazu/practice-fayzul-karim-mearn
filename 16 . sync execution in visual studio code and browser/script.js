f56eed39e7ef7459797e7fcbf6f1ec7e3a4c6853
Razu <jahangir8988@gmail.com>
2022-08-13 23:26:20 +0600

    ok

---
+++ 16 . sync execution in visual studio code and browser/script.js
@@ -0,0 +1,35 @@
+console.log('async file loaded')
+
+syncCallbackFunction = (index, start) => {
+    const numIsPrime = isPrime(index);
+    console.log('isprime', index, 'result: ', numIsPrime,'start: ', start, ' end: ', new Date())
+}
+
+const syncWithCallback = () => {
+    for(let index = 0; index < 10; index++){
+        // debugger;
+        console.log('looping', index, new Date()); //.getSeconds()
+        sleepWithCallback(index, syncCallbackFunction)
+    }
+}
+
+const sleepWithCallback = (num, callback) => {
+    const start = Date.now();
+	const sleep = num * 1000;
+    while(true){
+        if(Date.now() - start > sleep){break}
+    }
+    callback(num, new Date(start));
+}
+
+const isPrime = (num) => {
+    if(num < 2){return false;}
+    for(let i = 2; i < num; i++){
+        if(num % i === 0){return}
+    }
+    return true;
+}
+
+console.log(syncWithCallback()); //
+
+console.log('done')
