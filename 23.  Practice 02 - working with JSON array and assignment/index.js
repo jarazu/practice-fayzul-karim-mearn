832e82d8ada4d0a0fec8687b0e122df19ccbe465
MD Jahangir <jahangir@ad.tadlbd.com>
2022-08-17 22:14:24 +0600

    ok

---
+++ 23.  Practice 02 - working with JSON array and assignment/index.js
@@ -0,0 +1,9 @@
+console.log('script loaded');
+const student = {
+    "name":"John",
+    "age":20
+}
+const students = [student];
+const sumofagees = students.reduce((sum, current)=> sum+=current.age, 0)
+const avg = s
+console.log('done')
